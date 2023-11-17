e671bcf516e8d663cf91be17d5ab0a29b82fb407
Eric Kunzel <kunzel.eric@gmail.com>
2023-11-17 10:22:11 -0300

    feat: created user repository + create user route + error handling + configured test environment

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,33 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { RegisterUseCase } from '@/use-cases/register'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerUserBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, password, email } = registerUserBodySchema.parse(request.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+    await registerUseCase.execute({
+      name,
+      password,
+      email,
+    })
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    throw error
+  }
+
+  reply.status(201).send()
+}
---
+++ src/use-cases/register.ts
@@ -0,0 +1,34 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+// SOLID
+// D - Dependency Inversion Principle
+// A classe pode receber as dependências pelo seu construtor
+// Ao invés da classe INSTANCIAR a dependência, ela recebe a dependência
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ name, password, email }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+  }
+}
