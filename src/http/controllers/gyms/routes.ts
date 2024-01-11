61c4e3d255f48bd3ef860456306132f0bd7c0c47
Eric Kunzel <kunzel.eric@gmail.com>
2024-01-11 09:38:27 -0300

    feat: implement role based access

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,14 +1,15 @@
-import { verifyJWT } from '@/http/middlewares/verify-jwt'
-import { create } from './create'
 import { FastifyInstance } from 'fastify'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
+import { create } from './create'
 import { search } from './search'
 import { nearby } from './nearby'
 
 export async function gymsRoutes(app: FastifyInstance) {
-  // app.addHook('onRequest', verifyJWT)
+  app.addHook('onRequest', verifyJWT)
 
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
